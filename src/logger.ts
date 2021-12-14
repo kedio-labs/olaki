893fd50182305d808fb9b86208456c6537021115
Dogan Narinc <tepafoo@gmail.com>
2021-12-14 15:07:36 +0100

    Prefix logs with timestamp.
    This is achieved by prepending a timestamp string to the args passed to console.debug/log/warn/error

--- src/logger.ts
+++ src/logger.ts
@@ -23,9 +23,17 @@ if (!currentLogLevel) {
   throw new Error(errorMessage);
 }
 
+/* eslint-disable @typescript-eslint/ban-types */
+const logWithTimestamp = (consoleFn: Function) => {
+  return (...args: any[]) => {
+    const now = new Date();
+    return consoleFn(`${now.getTime()} - ${now.toISOString()}`, ...args);
+  };
+}; /* eslint-enable */
+
 export default {
-  error: currentLogLevel >= LogLevel.error ? console.error : noOp,
-  warn: currentLogLevel >= LogLevel.warn ? console.warn : noOp,
-  info: currentLogLevel >= LogLevel.info ? console.log : noOp,
-  debug: currentLogLevel >= LogLevel.debug ? console.debug : noOp,
+  error: currentLogLevel >= LogLevel.error ? logWithTimestamp(console.error) : noOp,
+  warn: currentLogLevel >= LogLevel.warn ? logWithTimestamp(console.warn) : noOp,
+  info: currentLogLevel >= LogLevel.info ? logWithTimestamp(console.log) : noOp,
+  debug: currentLogLevel >= LogLevel.debug ? logWithTimestamp(console.debug) : noOp,
 };
