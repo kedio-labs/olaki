63e1f893b0b5727c8fe3f25da97e15bcbfab4b59
Kédio Labs <dogan.narinc+kedio@gmail.com>
2024-10-30 10:20:41 +0100

    feat: refactoring of kali devices parsing

--- src/ts/device_summary_extractor/kali.ts
+++ src/ts/device_summary_extractor/kali.ts
@@ -1,49 +1,19 @@
-import appConfig from '../../../appConfig.json';
-import logger from '../../logger';
 import { CodenameToDeviceSummary } from './model';
 import { fetchUrl, normaliseCodename } from './util';
-import { load } from 'cheerio';
+import { load } from 'js-yaml';
+import logger from '../../logger';
+import appConfig from '../../../appConfig.json';
 
-const KALI_DEVICES_LIST_URL = 'https://stats.nethunter.com/images.html';
+const KALI_DEVICES_FILE_URL =
+  'https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-kernels/-/raw/main/devices.yml';
+
 const KALI_MOBILE_DOWNLOAD_URL = 'https://www.kali.org/get-kali/#kali-mobile';
 
-const extractDeviceVendorAndName = (codename: string, deviceName: string) => {
+const extractDeviceVendorAndName = (model: string) => {
   // device names from the device list include OS info
   // e.g. Nexus 6P (LineageOS 17.1)
   // let's remove that
-  const sanitisedDeviceName = deviceName.replace(/\(.+\)/, '').trim();
-
-  if (sanitisedDeviceName.toLowerCase().includes('nexus')) {
-    return {
-      vendor: 'Google',
-      name: sanitisedDeviceName,
-    };
-  }
-
-  // device: Pocophone F1
-  if (codename === 'beryllium') {
-    return {
-      vendor: 'Xiaomi',
-      name: sanitisedDeviceName,
-    };
-  }
-
-  // device: TicWatch Pro
-  if (codename === 'catfish') {
-    return {
-      vendor: 'Mobvoi',
-      name: sanitisedDeviceName,
-    };
-  }
-
-  // gts4llte: Galaxy Tab S4 LTE
-  // gts4lwifi: Galaxy Tab S4 WiFi
-  if (codename === 'gts4llte' || codename === 'gts4lwifi') {
-    return {
-      vendor: 'Samsung',
-      name: sanitisedDeviceName,
-    };
-  }
+  const sanitisedDeviceName = model.replace(/\(.+\)/, '').trim();
 
   const splitBySpace = sanitisedDeviceName.split(' ');
   return {
@@ -52,55 +22,36 @@ const extractDeviceVendorAndName = (codename: string, deviceName: string) => {
   };
 };
 
-const normaliseStatus = (status: string) => status.toLowerCase().trim();
-
-enum Status {
-  Stable = 'stable',
-  Latest = 'latest',
-}
-
 export default async function extractKaliDeviceSummaries(): Promise<CodenameToDeviceSummary> {
-  const codenameToDeviceSummary: CodenameToDeviceSummary = {};
-  const response = await fetchUrl('[KALI]', KALI_DEVICES_LIST_URL);
-
   logger.debug('[KALI] Scraping kali devices list');
-  const $ = load(response.data);
 
-  $('tr').each((index, trElement) => {
-    // rows on the devices list page are of the form
-    // | Display Name (Android OS) | Device (i.e. codename) | Kernel ID (ignore) | Android Version (ignore) | Rootfs (ignore) | Status | Documentation Link (ignore) | Notes (ignore) |
-    // | Nexus 5 (Marshmallow 6.x) | hammerhead             | hammerhead         | marshmallow 	            | full            | Stable | <empty>                     | Nexmon support |
-    // see https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-devices/-/blob/master/scripts/generate_images_table.py
+  const codenameToDeviceSummary: CodenameToDeviceSummary = {};
+  const response = await fetchUrl('[KALI]', KALI_DEVICES_FILE_URL);
 
-    // codename
-    const codename: string = $($(trElement).find('td').get(1)).text();
+  const codenameToDeviceArray: any = load(response.data, { json: true }) as any;
+
+  codenameToDeviceArray.forEach((codenameToDevice: any) => {
+    const codename: string = Object.keys(codenameToDevice)[0];
     const normalisedCodename = normaliseCodename(codename);
 
-    const status: string = normaliseStatus($($(trElement).find('td').get(5)).text());
-
+    const hasPreCreatedImages = !!codenameToDevice[codename].images;
     if (
-      (status === Status.Stable && appConfig.kali.includeStable) ||
-      // the devices list may contain multiple entries for the same codename (one for "stable" and another one for "latest")
-      // only include the stable one in that scenario
-      (status === Status.Latest && appConfig.kali.includeLatest && !codenameToDeviceSummary[normalisedCodename])
+      (hasPreCreatedImages && appConfig.kali.includePreCreatedImages) ||
+      (!hasPreCreatedImages && appConfig.kali.includeImagesToSelfGenerate)
     ) {
-      // device name may include vendor name
-      const deviceName: string = $($(trElement).find('td').get(0)).text();
-
-      const vendorAndName = extractDeviceVendorAndName(normalisedCodename, deviceName);
-
+      const deviceVendorAndName = extractDeviceVendorAndName(codenameToDevice[codename].model);
       codenameToDeviceSummary[normalisedCodename] = {
         codename: normalisedCodename,
-        vendor: vendorAndName.vendor,
-        name: vendorAndName.name,
+        name: deviceVendorAndName.name,
+        vendor: deviceVendorAndName.vendor,
         kali: {
+          hasPreCreatedImages: hasPreCreatedImages,
+          isSelfGeneratedImage: !hasPreCreatedImages,
           url: KALI_MOBILE_DOWNLOAD_URL,
-          isStable: status === Status.Stable,
-          isLatest: status === Status.Latest,
         },
       };
     } else {
-      logger.debug(`[KALI] Excluding codename: ${normalisedCodename} with status: ${status}`);
+      logger.debug(`[KALI] Excluding codename: ${normalisedCodename} with flag hasPreCreatedImages: ${hasPreCreatedImages}`);
     }
   });
 
