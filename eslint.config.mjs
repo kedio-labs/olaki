2b19c4d9928241a5163b9a2019c05b65ec3fadb3
Kédio Labs <140621664+kedio-labs@users.noreply.github.com>
2025-01-20 15:34:15 +0100

    chore: update dependency versions (#87)
    * chore: update dependency versions
    
    * chore: update eslint config accordingly to the new pattern introduced in v9

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+  {
+    plugins: {
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      indent: ['error', 2],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-ignore': 'allow-with-description',
+        },
+      ],
+    },
+  },
+];
