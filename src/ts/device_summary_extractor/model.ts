8fe7a78f212dc0adfd1361b1d9c95eab76e82a14
Kédio Labs <140621664+kedio-labs@users.noreply.github.com>
2025-08-27 16:41:18 +0200

    fix: update eos install modes (#98)

--- src/ts/device_summary_extractor/model.ts
+++ src/ts/device_summary_extractor/model.ts
@@ -39,9 +39,9 @@ export enum EOSMaturity {
 
 export enum EOSInstallMode {
   communityInstallDoc,
+  eOSInstaller,
+  murenaSmartphones,
   officialInstallDoc,
-  easyInstaller,
-  eSmartphones,
   rollBack,
 }
 
